f1da1f874118268c973fa5aeb56096fbbb3a75af
Mac <mac@Macs-MacBook-Air.local>
2026-03-04 13:48:43 +0200

    add user entity and CRUD endpoints

--- src/main/java/com/mahmoud/Application.java
+++ src/main/java/com/mahmoud/Application.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Application {
+
+    private final UserRepository repo;
+
+    public Application(UserRepository repo) {
+        this.repo = repo;
+    }
 
     @GetMapping("/")
     public String home() {
@@ -16,6 +24,16 @@ public class Application {
     @GetMapping("/boom")
     public String boom() {
         throw new RuntimeException("Test error");
+    }
+
+    @PostMapping("/users")
+    public User create(@RequestBody User user) {
+        return repo.save(user);
+    }
+
+    @GetMapping("/users")
+    public List<User> all() {
+        return repo.findAll();
     }
 
     public static void main(String[] args) {
